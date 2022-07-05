89a16a18409868a23d610046990bf294f68c3e23
sj-lee98 <denny00123@naver.com>
2022-07-05 11:00:02 +0900

    Initial Commit

---
+++ MustGoLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MustGoLogin
+//
+//  Created by 이성주 on 2022/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MustGoLogin/MustGoLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  MustGoLoginApp.swift
+//  MustGoLogin
+//
+//  Created by 이성주 on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct MustGoLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
