9587ae9864f9e425db6f50a1fd8a507bc0f450a1
sj-lee98 <denny00123@naver.com>
2022-07-12 20:12:58 +0900

    카카오 로그인 추가

--- MustGoLogin/ContentView.swift
+++ MustGoLogin/ContentView.swift
@@ -6,16 +6,229 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import KakaoSDKAuth
+import KakaoSDKUser
+import KakaoSDKCommon
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+class AppViewModel: ObservableObject {
+    
+    let auth = Auth.auth()
+    
+    @Published var signedIn = false
+    
+    var isSignedIn: Bool {
+        // current user 가 nil이 아니면 true return
+        return auth.currentUser != nil
+    }
+    
+    func signIn(email: String, password: String) {
+        auth.signIn(withEmail: email, password: password) { [weak self] result, error in
+            guard result != nil, error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                //Success
+                self?.signedIn = true
+            }
+        }
+    }
+    
+    func signUp(email: String, password: String) {
+        auth.createUser(withEmail: email, password: password) { [weak self] result, error in
+            guard result != nil, error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                //Success
+                self?.signedIn = true
+            }
+        }
+    }
+    
+    func signOut() {
+        try? auth.signOut()
+        
+        self.signedIn = false
     }
 }
+
+
+struct ContentView: View {
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        NavigationView {
+            if viewModel.signedIn {
+                VStack {
+                    Text("You are signed in")
+                    Button(action: {
+                        viewModel.signOut()
+                    }, label: {
+                        Text("Sign Out")
+                            .foregroundColor(Color.blue)
+                            .padding()
+                    })
+                }
+            }
+            else {
+                SignInView()
+            }
+        }
+        .onAppear {
+            viewModel.signedIn = viewModel.isSignedIn
+        }
+    }
+}
+
+
+
+struct SignInView: View {
+    @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        
+        VStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+            
+            VStack {
+                TextField("Email Address", text: $email)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                
+                
+                
+                Button(action: {
+                    
+                    guard !email.isEmpty, !password.isEmpty else {
+                        return
+                    }
+                    
+                    viewModel.signIn(email: email, password: password)
+                    
+                }, label: {
+                    Text("Sign In")
+                        .foregroundColor(Color.white)
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                })
+                
+                
+                Button(action : {
+                            //카카오톡이 깔려있는지 확인하는 함수
+                            if (UserApi.isKakaoTalkLoginAvailable()) {
+                                //카카오톡이 설치되어있다면 카카오톡을 통한 로그인 진행
+                                UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                                    print(oauthToken?.accessToken ?? "null")
+                                    print(error ?? "null")
+                                }
+                            }else{
+                                //카카오톡이 설치되어있지 않다면 사파리를 통한 로그인 진행
+                                UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                                    print(oauthToken?.accessToken ?? "null")
+                                    print(error ?? "null")
+                                }
+                            }
+                        }){
+                            
+                            Text("카카오 로그인")
+                                .padding()
+                        }
+                        //ios가 버전이 올라감에 따라 sceneDelegate를 더이상 사용하지 않게되었다
+                        //그래서 로그인을 한후 리턴값을 인식을 하여야하는데 해당 코드를 적어주지않으면 리턴값을 인식되지않는다
+                        //swiftUI로 바뀌면서 가장큰 차이점이다.
+                        .onOpenURL(perform: { url in
+                            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                                _ = AuthController.handleOpenUrl(url: url)
+                            }
+                        })
+                
+                NavigationLink("계정 만들기", destination: SignUpView())
+                    .padding()
+            }
+            .padding()
+            Spacer()
+        }
+        .navigationTitle("Sign In")
+    }
+}
+
+
+struct SignUpView: View {
+    @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        
+        VStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+            
+            VStack {
+                TextField("Email Address", text: $email)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                
+                
+                
+                
+                
+                Button(action: {
+                    
+                    guard !email.isEmpty, !password.isEmpty else {
+                        return
+                    }
+                    
+                    viewModel.signUp(email: email, password: password)
+                    
+                }, label: {
+                    Text("계정 만들기")
+                        .foregroundColor(Color.white)
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                })
+            }
+                        .padding()
+                        Spacer()
+            
+            
+            
+            
+        }
+        .navigationTitle("계정 만들기")
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
--- MustGoLogin/MustGoLoginApp.swift
+++ MustGoLogin/MustGoLoginApp.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
+import Firebase
+import KakaoSDKCommon
+import KakaoSDKAuth
+
 
 @main
 struct MustGoLoginApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
+            let viewModel = AppViewModel()
             ContentView()
+                .environmentObject(viewModel)
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    
+        
+        func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+               if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                   return AuthController.handleOpenUrl(url: url, options: options)
+               }
+               
+               return false
+           }
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        KakaoSDK.initSDK(appKey: "c9d615cffc2ce8b7e97b6b9fc449377a", loggingEnable:false)
+        return true
     }
 }
